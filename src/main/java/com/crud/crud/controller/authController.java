e0c45b5372af972ceb61b609bc1983e1918f28b1
Ashish <ashishkrgupta.7890@gmail.com>
2026-04-01 14:41:20 +0530

    second commit

---
+++ src/main/java/com/crud/crud/controller/authController.java
@@ -0,0 +1,25 @@
+package com.crud.crud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class authController {
+
+    @PostMapping("/{id}")
+    public String hello(){
+        return "Hello";
+    }
+
+    @GetMapping ("/{id}")
+    public String GetEmployee(){
+        return "Hello Ashish";
+    }
+
+
+    @GetMapping ("/{employeeId}")
+    public String GetEmployeeById(){
+        return "checking kar raha hu";
+    }
+}
